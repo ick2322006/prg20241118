665e5c951817611da44443c718c62d6b65beb314
ick2322006 <ick2322006@ichiko.ed.jp>
2026-01-06 17:48:13 +0900

    kadaikenkyu_date_4

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-const { NFC } = require('nfc-pcsc');
-const nfc = new NFC();
+SUBSYSTEM=="usb", ACTION=="add", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="06c1", GROUP="plugdev", MODE="0660"
+SUBSYSTEM=="usb", ACTION=="add", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="06c3", GROUP="plugdev", MODE="0660"
 
-console.log('--- [診断開始] ---');
-console.log('1. NFCライブラリを初期化しました。');
-
-nfc.on('reader', reader => {
-    // ここが表示されれば、ラズパイがリーダーを「認識」できています
-    console.log(`2. リーダーを検出しました!: ${reader.reader.name}`);
-
-    reader.on('card', card => {
-        // ここが表示されれば、カードの読み取り成功です
-        console.log(`3. カードを検出!: ID = ${card.uid}`);
-    });
-
-    reader.on('error', err => {
-        console.log(`X. リーダーエラー発生: ${err}`);
-    });
-
-    reader.on('card.off', () => {
-        console.log('4. カードが離れました。');
-    });
-});
-
-nfc.on('error', err => {
-    console.log(`X. システム全体のエラー: ${err}`);
-});
-
-// 5秒経っても何も起きない場合
-setTimeout(() => {
-    console.log('--- [5秒経過] もし「2. リーダーを検出」が出ていなければ、OSがリーダーを認識できていません ---');
-}, 5000);
